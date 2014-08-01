4b9245b34a313f3fc295c37b3eb1ebf330691e36
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-08-01 11:22:01 +0100

    Expressions using strings and numbers

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 // This is where all the practise code from the Introduction to Javascript tutorial goes
 
-console.log('Hello, this is my first line of JavaScrip using Codebar');
+console.log('Hello, this is my first line of JavaScript using Codebar');
+
+// In JavaScript you always need to write var before the variable name
+var name = "Codebar" ;
+console.log(name + " is amazing!");
+
+// JavaScript will always try to convert everything into a string.
+// In the example below JavaScript will treat the number as a string and will concatenate it to the previous string.
+
+var pi = 3.14;
+console.log("The value of pi is " + pi);
+
+var year = 2014
+var nextYear = year + 1;
+console.log("We are in " + year + " but " + nextYear + " is just around the corner!");
