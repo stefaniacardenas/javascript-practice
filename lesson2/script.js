85e522c69557e1d8ac088d795cb9c8d278008bea
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-08-03 11:27:15 +0100

    While loop

--- lesson2/script.js
+++ lesson2/script.js
@@ -1,2 +1,12 @@
 // used in lesson2.html
 
+// While loop - it is executed until the condition return false.
+var i = 1;
+var total = 0;
+
+while (i <= 10) {
+	total = total + i;
+	i = i + 1;
+}
+
+console.log("Total: " + total);
